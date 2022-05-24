048c252a5aa706b133cf238cc04ccf35302b658d
Ash Crockett <crockett.ash@gmail.com>
2022-05-24 20:14:38 +1200

    Initial commit.

---
+++ apps/storybook/.storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../../codewizardry/**/*.stories.txs",
+    "../../../shared/papyrus/**/*.stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": '@storybook/react'
+}
